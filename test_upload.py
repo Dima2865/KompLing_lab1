0a468190e8a5144626a67a46e9383d6478941d13
Dima2865 <100437197+Dima2865@users.noreply.github.com>
2025-02-28 11:58:48 +0300

    Update test_upload.py

--- test_upload.py
+++ test_upload.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'http://127.0.0.1:8080/upload'
-files = [('files', open('Safonova.m4a', 'rb')), ('files', open('examples_data_001ce26c07c20eaa0d666b824c6c6924.wav', 'rb'))]
+files = [('files', open('Safonova.m4a', 'rb')), ('files', open('001ce26c07c20eaa0d666b824c6c6924.wav', 'rb'))]
 resp = requests.post(url=url, files = files) 
 print(resp.json())
 
